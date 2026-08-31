88572ee71dbc3d9a0b9475fd566260117f6610e8
Abdelrahman Nader <abdelrahmannohman33@gmail.com>
2026-08-31 22:07:18 +0300

    Implement login and signup endpoint along with logic

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import habitsRoutes from './routes/habitsRoutes.js';
+import accountRoutes from './routes/accountRoutes.js';
 import { connectDB } from "./config/db.js";
 import cors from 'cors';
 dotenv.config();
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/habits", habitsRoutes);
+app.use("/api/account", accountRoutes);
 
 connectDB().then(() =>
 { app.listen(5001, () => {
